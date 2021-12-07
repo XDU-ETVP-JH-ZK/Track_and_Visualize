9b66450734788177c1fbb34ad393f2a224f314a8
zk8514 <851429737@qq.com>
2021-12-07 16:53:46 +0800

    upload

---
+++ 静态库源码/可视化/pangolin_lib.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#ifndef __pangolinlib__
+#define __pandolinlib__
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+//using namespace std;
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+//using namespace Eigen;
+
+#include <pangolin/pangolin.h>
+//多线程的库
+#include <thread>
+#include <regex>
+
+void pview(std::string file);
+
+#endif
